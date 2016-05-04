b43a0007e2ef40c373d7a1e32744eda5543184a4
Stephane Nicoll <snicoll@pivotal.io>
2016-05-04 15:31:55 +0200

    Remove Spring Data REST

--- src/main/java/com/example/cfp/domain/SpeakerRepository.java
+++ src/main/java/com/example/cfp/domain/SpeakerRepository.java
@@ -3,18 +3,13 @@ package com.example.cfp.domain;
 import java.util.Collection;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface SpeakerRepository extends CrudRepository<Speaker, Long> {
 
-	@RestResource(path = "by-twitter")
-	Speaker findByTwitter(@Param("id") String twitter);
+	Speaker findByTwitter(String twitter);
 
-	@RestResource(path = "by-github")
-	Speaker findByGithub(@Param("id") String github);
+	Speaker findByGithub(String github);
 
-	@RestResource(path = "by-name")
-	Collection<Speaker> findByName(@Param("name") String lastName);
+	Collection<Speaker> findByName(String lastName);
 
 }
