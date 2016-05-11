4ae8fd9138185060400b6e61609dcec3bb9168cf
Stephane Nicoll <snicoll@pivotal.io>
2016-05-11 13:16:18 +0200

    Externalize configuration
    This commit moves the hard-coded list of admin users to a configurable
    property using a type-safe `@ConfigurationProperties` POJO. This commit
    also adds the annotation processor so that the relevant meta-data is
    generated at build time for IDE assistance.
    
    If we remove one of the expected admin from the list, you get a 403 so
    we could create a dedicated error page for that. Since we want a general
    client-side error page, we rename our 404 to `4xx` so that Boot uses it
    for any client-side error by default.

---
+++ src/main/java/com/example/cfp/CfpProperties.java
@@ -0,0 +1,35 @@
+package com.example.cfp;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("cfp")
+public class CfpProperties {
+
+	private final Security security = new Security();
+
+	public Security getSecurity() {
+		return this.security;
+	}
+
+
+	public static class Security {
+
+		/**
+		 * Github users that have admin rights.
+		 */
+		private List<String> admins = Arrays.asList("snicoll", "bclozel");
+
+		public List<String> getAdmins() {
+			return admins;
+		}
+
+		public void setAdmins(List<String> admins) {
+			this.admins = admins;
+		}
+
+	}
+
+}
